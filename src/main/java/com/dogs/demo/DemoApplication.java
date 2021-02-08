61223da45d91e36f716dabe3cfe1a23428c4bde7
Reda Tawfik <reda.rm148@gmail.com>
2021-02-08 19:24:22 +0200

    add log

--- src/main/java/com/dogs/demo/DemoApplication.java
+++ src/main/java/com/dogs/demo/DemoApplication.java
@@ -1,11 +1,23 @@
 package com.dogs.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 
 @SpringBootApplication
 public class DemoApplication {
+
+    public static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+    @PostConstruct
+    public void init() {
+        logger.info("Application started successfully");
+        logger.info("Application started");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
